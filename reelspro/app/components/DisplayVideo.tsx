5571d56177e173d2448653aa73881af1906939d0
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-02-19 20:44:06 +0530

    components
    components

---
+++ reelspro/app/components/DisplayVideo.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function DisplayVideo() {
+  return <div>DisplayVideo</div>;
+}
+
+export default DisplayVideo;
+
+// import React from 'react';
+
+// function DisplayVideo(video) {
+//   return (
+//     <div>
+//       <video src="video" height={320} width={180} />
+//     </div>
+//   );
+// }
+
+// export default DisplayVideo;
+
+{
+  /* <div className="card bg-base-100 image-full w-96 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes"
+    />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> */
+}
