04e740b747e69bba0fa3fddff7594d6670f9465e
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-05-02 09:47:02 +0530

    dummyUsersNTechs json
    dummyUsersNTechs json

---
+++ users-mgmt-poc/src/app/api/dummyUsersNTechs.js
@@ -0,0 +1,24 @@
+export const dummyUsersNTechs = {
+    users: [
+      {
+        id: 1,
+        courses: [10, 21],
+      }, 
+      {
+        id: 2,
+        courses: [10, 21],
+      }, 
+      {
+        id: 3,
+        courses: [21, 31],
+      },
+      {
+        id: 4,
+        courses: [20, 31],
+      },
+      {
+        id: 5,
+        courses: [10, 20],
+      },
+    ]
+}
