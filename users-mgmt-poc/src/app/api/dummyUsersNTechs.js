b5c34c8073810ab178b24936d68885375e904092
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-05-12 00:54:07 +0530

    .
    .

--- users-mgmt-poc/src/app/api/dummyUsersNTechs.js
+++ users-mgmt-poc/src/app/api/dummyUsersNTechs.js
@@ -20,5 +20,25 @@ export const dummyUsersNTechs = {
         id: 5,
         courses: [10, 20],
       },
+      {
+        id: 6,
+        courses: [],
+      },
+      {
+        id: 7,
+        courses: [],
+      },
+      {
+        id: 8,
+        courses: [],
+      },
+      {
+        id: 9,
+        courses: [],
+      },
+      {
+        id: 10,
+        courses: [],
+      },
     ]
 }
