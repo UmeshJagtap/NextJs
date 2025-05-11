a734fa68dde0c746ad24b6664920fa9e99919af7
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-05-11 17:01:30 +0530

    radio button section
    radio button section

---
+++ users-mgmt-poc/src/app/users/testPage.tsx
@@ -0,0 +1,35 @@
+{
+  /* <section className="p-2 m-2 flex gap-2 content-center">
+            <input
+              type="radio"
+              id="admin"
+              name="roleFilter"
+              value="admin"
+              checked={roleFilter === 'admin'}
+              onChange={(e) => setRoleFilter(e.target.value)}
+            />
+            <label htmlFor="admin">Admin</label>
+
+            <input
+              type="radio"
+              id="moderator"
+              name="roleFilter"
+              value="moderator"
+              checked={roleFilter === 'moderator'}
+              onChange={(e) => setRoleFilter(e.target.value)}
+            />
+            <label htmlFor="moderator">Moderator</label>
+
+            <input
+              type="radio"
+              id="user"
+              name="roleFilter"
+              value="user"
+              checked={roleFilter === 'user'}
+              onChange={(e) => setRoleFilter(e.target.value)}
+            />
+            <label htmlFor="user">User</label>
+
+            {JSON.stringify(roleFilter)}
+          </section> */
+}
