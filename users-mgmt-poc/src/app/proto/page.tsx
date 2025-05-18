b6aa29ee1362f4b8ce4ef6af9bc00f5abf89635c
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-05-18 20:34:09 +0530

    .
    .

--- users-mgmt-poc/src/app/proto/page.tsx
+++ users-mgmt-poc/src/app/proto/page.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const Proto = () => {
   return (
-    <div className='bg-[#1B1212] text-[#E2DFD2]'>
-        <section className='m-4 p-4 flex flex-col items-end justify-end text-8xl gap-4'>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>WORKS</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>ABOUT</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>CONTACT</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>RESUME</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>BLOG</p>
-
-        </section>
+    <div className="bg-[#1B1212] text-[#E2DFD2]">
+      <p>X</p>
+      <section className="m-4 p-4 flex flex-col items-end justify-end text-8xl gap-4">
+        <p className="border-b border-[#121212] hover:border-[#E2DFD2]">
+          WORKS
+        </p>
+        <p className="border-b border-[#121212] hover:border-[#E2DFD2]">
+          ABOUT
+        </p>
+        <p className="border-b border-[#121212] hover:border-[#E2DFD2]">
+          CONTACT
+        </p>
+        <p className="border-b border-[#121212] hover:border-[#E2DFD2]">
+          RESUME
+        </p>
+        <p className="border-b border-[#121212] hover:border-[#E2DFD2]">BLOG</p>
+      </section>
     </div>
-  )
-}
+  );
+};
 
 export default Proto;
