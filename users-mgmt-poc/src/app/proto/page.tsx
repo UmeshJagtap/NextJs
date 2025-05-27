7dd468a8b5f319e05f49cd12b59be6a275b23607
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-05-28 01:22:54 +0530

    added underline-animate
    added underline-animate

--- users-mgmt-poc/src/app/proto/page.tsx
+++ users-mgmt-poc/src/app/proto/page.tsx
@@ -1,17 +1,48 @@
-import React from 'react'
-
+'use client';
+import Image from 'next/image';
+import './underline-animate.css';
 const Proto = () => {
   return (
-    <div className='bg-[#1B1212] text-[#E2DFD2]'>
+    <section className=''>
+      <div className='bg-[#222831] text-[#DFD0B8]'>
         <section className='m-4 p-4 flex flex-col items-end justify-end text-8xl gap-4'>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>WORKS</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>ABOUT</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>CONTACT</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>RESUME</p>
-            <p className='border-b border-[#121212] hover:border-[#E2DFD2]'>BLOG</p>
+          <p className='underline-animate'>WORKS</p>
+          <p className='underline-animate'>ABOUT</p>
+          <p className='border-b border-[#121212] hover:border-[#E2DFD2] underline-animate'>CONTACT</p>
+          <p className='border-b border-[#121212] hover:border-[#E2DFD2] underline-animate'>RESUME</p>
+          <p className='border-b border-[#121212] hover:border-[#E2DFD2] underline-animate'>BLOG</p>
 
         </section>
-    </div>
+        <section className='p-2 m-2'>
+          <article>
+            <time>Mar 10, 2020</time>
+            <h2>Boost your conversion rate</h2>
+            <p className="line-clamp-3">
+              Nulla dolor velit adipisicing duis excepteur esse in duis nostrud occaecat mollit incididunt deserunt sunt. Ut ut
+              sunt laborum ex occaecat eu tempor labore enim adipisicing minim ad. Est in quis eu dolore occaecat excepteur fugiat
+              dolore nisi aliqua fugiat enim ut cillum. Labore enim duis nostrud eu. Est ut eiusmod consequat irure quis deserunt
+              ex. Enim laboris dolor magna pariatur. Dolor et ad sint voluptate sunt elit mollit officia ad enim sit consectetur
+              enim.
+            </p>
+            <div className='flex items-center gap-2'>
+              <p className='text-[#E2DFD2]'>Lindsay Walton</p>
+            <div className='w-10 h-10 rounded-full bg-red-500'></div>
+            <div className='w-14 h-14 rounded-full bg-red-500'></div>
+            <div className='w-20 h-20 rounded-full bg-red-500'></div>
+              <div className='my-2 rounded-full overflow-hidden'>
+              {/* <Image src="/img/lindsay.jpg" alt='Lindsay Walton' width={80} height={50} /> */}
+              <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3F6VEGUwo6MNIFBKieE81JDvSQa6HnauiqA&s" alt='Lindsay Walton' width={80} height={50} />
+            </div>
+            </div>
+            
+          </article>
+        </section>
+      </div>
+
+     
+
+    </section>
+
   )
 }
 
