d3cc3190d7c3c4bfc459392254ac9d4091e470ac
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-05-28 01:23:32 +0530

    Fullscreen menu
    Fullscreen menu

---
+++ users-mgmt-poc/src/app/proto2/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./Proto2.css";
+
+const Proto2 = () => {
+  return (
+    <div>
+      <main style={{ padding: "100px" }}>
+        <h1>
+          Fullscreen menu
+          <span> with cool links </span>
+        </h1>
+        <input type="checkbox" id="myInput" />
+        <label htmlFor="myInput">
+          <span className="bar top"></span>
+          <span className="bar middle"></span>
+          <span className="bar bottom"></span>
+        </label>
+        <aside>
+          <div className="aside-section aside-left">
+            <div className="aside-content">
+              <p> Some text that will make you click the cta </p>
+              <button className="button"> CTA </button>
+            </div>
+          </div>
+          <div className="aside-section aside-right">
+            <ul className="aside-list">
+              <li>
+                <a href="" className="aside-anchor">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="aside-anchor">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="aside-anchor">
+                  Link
+                </a>
+              </li>
+              <li>
+                <a href="" className="aside-anchor">
+                  Link
+                </a>
+              </li>
+            </ul>
+          </div>
+        </aside>
+      </main>
+    </div>
+  );
+};
+
+export default Proto2;
