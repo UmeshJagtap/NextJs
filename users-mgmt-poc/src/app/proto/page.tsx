8c8c72d56ba7fca231c5a30e7584235cd3a89ad5
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2025-06-07 20:05:46 +0530

    .
    .

--- users-mgmt-poc/src/app/proto/page.tsx
+++ users-mgmt-poc/src/app/proto/page.tsx
@@ -24,3 +24,13 @@ const Proto = () => {
 };
 
 export default Proto;
+
+{
+  /* <section className="m-4 p-4 flex flex-col items-end justify-end text-8xl gap-4">
+  <p className="border-b border-[#121212] hover:border-[#E2DFD2]">WORKS</p>
+  <p className="border-b border-[#121212] hover:border-[#E2DFD2]">ABOUT</p>
+  <p className="border-b border-[#121212] hover:border-[#E2DFD2]">CONTACT</p>
+  <p className="border-b border-[#121212] hover:border-[#E2DFD2]">RESUME</p>
+  <p className="border-b border-[#121212] hover:border-[#E2DFD2]">BLOG</p>
+</section>; */
+}
